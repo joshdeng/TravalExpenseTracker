63f02b7e08fca7bda0428b71d5fa9d27cec1e45c
Xindong Deng <j.deng7@hotmail.com>
2015-01-14 01:02:31 -0700

    working on interface

--- travelexpensetracking/gen/ca/ualberta/cs/travelexpensetracking/R.java
+++ travelexpensetracking/gen/ca/ualberta/cs/travelexpensetracking/R.java
@@ -10,6 +10,15 @@ package ca.ualberta.cs.travelexpensetracking;
 public final class R {
     public static final class attr {
     }
+    public static final class color {
+        public static final int Brown=0x7f040006;
+        public static final int darkblue=0x7f040002;
+        public static final int gray=0x7f040005;
+        public static final int red=0x7f040004;
+        public static final int sand=0x7f040000;
+        public static final int skyblue=0x7f040001;
+        public static final int white=0x7f040003;
+    }
     public static final class dimen {
         /**  Default screen margins, per the Android Design guidelines. 
 
@@ -18,28 +27,38 @@ public final class R {
          would include 7" and 10" devices in landscape (~960dp and ~1280dp respectively).
     
          */
-        public static final int activity_horizontal_margin=0x7f040000;
-        public static final int activity_vertical_margin=0x7f040001;
+        public static final int activity_horizontal_margin=0x7f050000;
+        public static final int activity_vertical_margin=0x7f050001;
     }
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int AddClaimButton=0x7f080002;
-        public static final int action_settings=0x7f080003;
-        public static final int editText1=0x7f080001;
-        public static final int scrollView1=0x7f080000;
+        public static final int AddClaimButton=0x7f090003;
+        public static final int ClaimListView=0x7f090001;
+        public static final int ClaimText=0x7f090002;
+        public static final int action_settings=0x7f090004;
+        public static final int textView1=0x7f090000;
     }
     public static final class layout {
-        public static final int activity_main=0x7f030000;
+        public static final int activity_add_expense=0x7f030000;
+        public static final int activity_claim=0x7f030001;
+        public static final int activity_email_claim=0x7f030002;
+        public static final int activity_main=0x7f030003;
     }
     public static final class menu {
-        public static final int main=0x7f070000;
+        public static final int add_expense=0x7f080000;
+        public static final int claim=0x7f080001;
+        public static final int email_claim=0x7f080002;
+        public static final int main=0x7f080003;
     }
     public static final class string {
-        public static final int action_settings=0x7f050002;
-        public static final int app_name=0x7f050000;
-        public static final int hello_world=0x7f050001;
+        public static final int action_settings=0x7f060002;
+        public static final int app_name=0x7f060000;
+        public static final int hello_world=0x7f060001;
+        public static final int title_activity_add_expense=0x7f060004;
+        public static final int title_activity_claim=0x7f060003;
+        public static final int title_activity_email_claim=0x7f060005;
     }
     public static final class style {
         /** 
@@ -63,10 +82,10 @@ public final class R {
     
  API 14 theme customizations can go here. 
          */
-        public static final int AppBaseTheme=0x7f060000;
+        public static final int AppBaseTheme=0x7f070000;
         /**  Application theme. 
  All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static final int AppTheme=0x7f060001;
+        public static final int AppTheme=0x7f070001;
     }
 }
