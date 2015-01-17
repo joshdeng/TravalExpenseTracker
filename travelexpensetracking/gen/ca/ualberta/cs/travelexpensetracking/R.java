dff4df3bbc7f8190462a954061b3c10235ebdba9
Xindong Deng <j.deng7@hotmail.com>
2015-01-16 22:04:35 -0700

    expense class finished

--- travelexpensetracking/gen/ca/ualberta/cs/travelexpensetracking/R.java
+++ travelexpensetracking/gen/ca/ualberta/cs/travelexpensetracking/R.java
@@ -35,12 +35,12 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int ClaimListView=0x7f090001;
         public static final int ClaimText=0x7f090002;
         public static final int ClaimsTextView1=0x7f090004;
         public static final int action_settings=0x7f090007;
         public static final int addClaimButton=0x7f090006;
         public static final int addExpenseButton=0x7f090003;
+        public static final int datePicker1=0x7f090001;
         public static final int listView1=0x7f090005;
         public static final int textView1=0x7f090000;
     }
